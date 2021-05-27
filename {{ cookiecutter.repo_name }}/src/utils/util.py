0a5ec8408cb4a57af9d74f0f95f3782f92c4cd98
Danilo Dorotheu <danilo.dorotheu@live.com>
2021-05-27 11:45:25 -0300

    first commit

---
+++ {{ cookiecutter.repo_name }}/src/utils/util.py
@@ -0,0 +1,19 @@
+import yaml
+import os
+
+def getParams(env: str = "loc", params: dict = {}) -> dict:
+    """
+        Retorna os parametros do projeto
+
+        Parameters
+        ----------
+            env:    str  (optional) Ambiente de execucao
+            params: dict (optional) Parametros adicionais
+
+        Returns
+        -------
+            dict
+    """
+    path_conf = os.path.abspath(os.path.dirname(__file__) + "\\..\\..\\config\\")
+
+    return yaml.load(open(f"{path_conf}\{env}-params.yaml"), Loader=yaml.FullLoader)
---
+++ {{ cookiecutter.repo_name }}/src/wrapper.py
@@ -0,0 +1,4 @@
+from utils import util
+
+if __name__ == '__main__':
+    print(util.getParams())
